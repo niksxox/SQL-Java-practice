5bc2cec27f68e487b3e6bea320867a9c37c6b3b8
Nikita Singh <152859100+niksxox@users.noreply.github.com>
2026-08-25 11:01:09 +0530

    Add CodeChef solution: Welcome to the CodeChef AI Tutor

---
+++ CodeChef/SQL/Welcome_to_the_CodeChef_AI_Tutor_6.sql
@@ -0,0 +1,11 @@
+// CodeChef
+// Problem: Welcome to the CodeChef AI Tutor
+// Language: SQL
+
+No problem description detected.
+
+// Solution
+
+-- your code goes here
+select book_id,title, author, published_year from library where rating 
+    isnull;
