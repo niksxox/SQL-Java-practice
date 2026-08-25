28b71d9c6e80147b0821de9d05cafc84e2e9d571
Nikita Singh <152859100+niksxox@users.noreply.github.com>
2026-08-25 10:59:52 +0530

    Add CodeChef solution: Welcome to the CodeChef AI Tutor

---
+++ CodeChef/SQL/Welcome_to_the_CodeChef_AI_Tutor.sql
@@ -0,0 +1,10 @@
+// CodeChef
+// Problem: Welcome to the CodeChef AI Tutor
+// Language: SQL
+
+No problem description detected.
+
+// Solution
+
+-- your code goes here
+select product_name, category from products where price>100;
