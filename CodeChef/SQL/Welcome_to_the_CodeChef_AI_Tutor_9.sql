f8e6bd461327b4c515875a9a539f3c5d44e19cd0
Nikita Singh <152859100+niksxox@users.noreply.github.com>
2026-08-25 16:26:31 +0530

    Add CodeChef solution: Welcome to the CodeChef AI Tutor

---
+++ CodeChef/SQL/Welcome_to_the_CodeChef_AI_Tutor_9.sql
@@ -0,0 +1,10 @@
+// CodeChef
+// Problem: Welcome to the CodeChef AI Tutor
+// Language: SQL
+
+No problem description detected.
+
+// Solution
+
+select author_id, author_name, publication_name from views where view_count = 0 group 
+    by author_id;
