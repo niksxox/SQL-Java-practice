5cc93b300f363a9ef2e69be4de596c7bd00ae32f
Nikita Singh <152859100+niksxox@users.noreply.github.com>
2026-08-25 11:00:56 +0530

    Add CodeChef solution: Welcome to the CodeChef AI Tutor

---
+++ CodeChef/SQL/Welcome_to_the_CodeChef_AI_Tutor_5.sql
@@ -0,0 +1,10 @@
+// CodeChef
+// Problem: Welcome to the CodeChef AI Tutor
+// Language: SQL
+
+No problem description detected.
+
+// Solution
+
+-- your code goes here
+select movie_name from cinema where rating>7.0 and rating<9.0;
