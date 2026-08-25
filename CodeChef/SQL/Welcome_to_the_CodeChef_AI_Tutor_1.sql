0fc99e480bad41efc472ef9b8b10814a1353229d
Nikita Singh <152859100+niksxox@users.noreply.github.com>
2026-08-25 11:00:11 +0530

    Add CodeChef solution: Welcome to the CodeChef AI Tutor

---
+++ CodeChef/SQL/Welcome_to_the_CodeChef_AI_Tutor_1.sql
@@ -0,0 +1,10 @@
+// CodeChef
+// Problem: Welcome to the CodeChef AI Tutor
+// Language: SQL
+
+No problem description detected.
+
+// Solution
+
+-- your code goes here
+select avg(salary) as avg_salary from works;
