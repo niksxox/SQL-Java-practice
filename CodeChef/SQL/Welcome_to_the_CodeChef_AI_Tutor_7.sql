aecacae2b4ddbae50d32f3dda07c4013389fc45b
Nikita Singh <152859100+niksxox@users.noreply.github.com>
2026-08-25 11:01:18 +0530

    Add CodeChef solution: Welcome to the CodeChef AI Tutor

---
+++ CodeChef/SQL/Welcome_to_the_CodeChef_AI_Tutor_7.sql
@@ -0,0 +1,11 @@
+// CodeChef
+// Problem: Welcome to the CodeChef AI Tutor
+// Language: SQL
+
+No problem description detected.
+
+// Solution
+
+--your code goes here
+select employee_name, company, salary from employees where category = "Full
+    -Time" order by salary desc;
